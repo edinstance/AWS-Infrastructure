a8fb6b3d8b5df87516725f342759062965b12c14
edinstance <edinstance@gmail.com>
2024-08-19 09:52:36 +0100

    feat: added internet gateway to public route table

--- Terraform/Modules/vpc/main.tf
+++ Terraform/Modules/vpc/main.tf
@@ -20,6 +20,10 @@ resource "aws_internet_gateway" "igw" {
 # This creates a public route table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
   tags = {
     Name = "${var.vpc_name}-public-rt"
   }
