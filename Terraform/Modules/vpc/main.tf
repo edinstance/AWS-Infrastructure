5d484f73e35d6f5fbf7c58a783f9d494216a0684
edinstance <edinstance@gmail.com>
2024-08-19 18:11:46 +0100

    fix: changed subnet config based on security scan result

--- Terraform/Modules/vpc/main.tf
+++ Terraform/Modules/vpc/main.tf
@@ -44,7 +44,7 @@ resource "aws_subnet" "public_subnet" {
   count                   = length(var.public_subnet_cidrs)
   cidr_block              = element(var.public_subnet_cidrs, count.index)
   availability_zone       = element(var.availability_zones, count.index)
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "public-${count.index}"
