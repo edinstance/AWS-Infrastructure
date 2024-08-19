1ff69ae240ad5acc82439743518735b7f3e4e126
edinstance <edinstance@gmail.com>
2024-08-19 18:15:17 +0100

    feat: added TODO for future vpc config

--- Terraform/Modules/vpc/main.tf
+++ Terraform/Modules/vpc/main.tf
@@ -10,6 +10,9 @@ terraform {
   }
 }
 
+# TODO: Consider adding VPC Flow Logs (MEDIUM Security Risk)
+# https://avd.aquasec.com/misconfig/avd-aws-0178
+
 # This creates a VPC with CIDR block
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
