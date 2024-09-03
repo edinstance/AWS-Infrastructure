4e71c755f746b55f676dcf049d87668c351d6ea3
edinstance <edinstance@gmail.com>
2024-09-03 02:20:28 +0100

    feat: added new providers and updated lock file

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -5,6 +5,14 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.60.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.5.0"
+    }
   }
 }
 
